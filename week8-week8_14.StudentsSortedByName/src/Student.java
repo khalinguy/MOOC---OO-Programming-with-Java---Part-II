17815e09c4beb177abe1165c68416ff772c16296
Khanh Linh Nguyen <58554649+khalinguy@users.noreply.github.com>
2019-12-20 15:20:28 +0200

    Add files via upload

---
+++ week8-week8_14.StudentsSortedByName/src/Student.java
@@ -0,0 +1,21 @@
+public class Student implements Comparable<Student> {
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    public int compareTo(Student student){
+        return name.compareToIgnoreCase(student.getName());
+    }
+}
