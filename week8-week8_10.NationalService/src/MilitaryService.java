f2e1ad8a448971f11e8a146cd86fd9f88f8d9ee3
Khanh Linh Nguyen <58554649+khalinguy@users.noreply.github.com>
2019-12-20 14:32:37 +0200

    Add files via upload

---
+++ week8-week8_10.NationalService/src/MilitaryService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Linh Nguyen
+ */
+public class MilitaryService implements NationalService {
+    private int daysLeft;
+    public MilitaryService(int daysLeft){
+        this.daysLeft = daysLeft;
+    }
+    public int getDaysLeft(){
+        return daysLeft;
+    }
+    
+    public void work(){
+        if (this.daysLeft > 0){
+            this.daysLeft --;
+        }
+    }
+}
